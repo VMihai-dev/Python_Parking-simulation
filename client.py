c6cce43cbe8dd317335dc51251e248426987b8be
VMihai-dev <iammihaivisan@gmail.com>
2020-11-01 13:44:48 +0000

    Switched to an echo client functionality with the ability of a client to hold a connection open.

--- client.py
+++ client.py
@@ -1,6 +1,22 @@
 # client.py
 import socket
 
+import socket
+import sys
+
+host = '127.0.0.1'
+port = 6565                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+while True:
+    for line in sys.stdin:
+        line = line.encode()
+        s.send(line)
+        data = s.recv(1024)
+        print('Received', repr(data))
+s.close()
+
+"""
 # create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -18,3 +34,4 @@ tm = s.recv(1024)
 s.close()
 
 print("The time got from the server is %s" % tm.decode('ascii'))
+"""
--- server.py
+++ server.py
@@ -2,6 +2,21 @@
 import socket
 import time
 
+host = '127.0.0.1'      # Symbolic name meaning all available interfaces
+port = 6565     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
+
+
+"""
 # create a socket object
 serversocket = socket.socket(
 	        socket.AF_INET, socket.SOCK_STREAM)
@@ -25,3 +40,4 @@ while True:
     currentTime = time.ctime(time.time()) + "\r\n"
     clientsocket.send(currentTime.encode('ascii'))
     clientsocket.close()
+"""
