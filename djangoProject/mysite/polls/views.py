bdf619c2dc48d7a33ecdf711c65bf3ff42ea21eb
VMihai-dev <iammihaivisan@gmail.com>
2020-11-02 15:05:23 +0000

    Django templates and variable passing basics.

--- djangoProject/mysite/polls/views.py
+++ djangoProject/mysite/polls/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from .... import server
 
+availableParking = 5
+context = {
+    'spaces': availableParking
+}
+def home(request):
+    return render(request, 'polls/home.html')
+# Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def about(request):
+    return render(request, 'polls/about.html', context)
 # Create your views here.
