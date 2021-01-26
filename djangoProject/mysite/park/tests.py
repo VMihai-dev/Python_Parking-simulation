0b890a46e12251281e8dddf41ed2326c74cf3d0b
VMihai-dev <iammihaivisan@gmail.com>
2021-01-26 09:08:57 +0000

    Made the home page look a bit better

--- djangoProject/mysite/park/tests.py
+++ djangoProject/mysite/park/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class MyTest(TestCase):
+
+    def test_pass(self):
+        self.assertEqual(1, 1)
+
+    def test_fail(self):
+        self.assertEqual(2, 1)
